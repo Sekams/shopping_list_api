87eb46064a268c4b9ea61ac38a774000be6ff81f
Simon Peter Ssekamatte <spssekamatte@gmail.com>
2017-08-25 06:31:38 +0300

    Add functionality for API
    Add functionality for the login, logout, reset password and shopping
    list endpoints

--- server/auth/views.py
+++ server/auth/views.py
@@ -1,8 +1,8 @@
-from . import auth_blueprint
+from . import auth_blueprint, shoppinglists_blueprint
 
 from flask.views import MethodView
-from flask import make_response, request, jsonify, json
-from server.models import db, User, BlacklistToken, Bcrypt
+from flask import make_response, request, jsonify, json, abort
+from server.models import db, User, ShoppingList, BlacklistToken, Bcrypt
 
 
 class RegisterAPI(MethodView):
@@ -144,7 +144,8 @@ class ResetPasswordAPI(MethodView):
             if not isinstance(user_id, str):
                 user = User.query.filter_by(id=user_id).first()
                 if user and user.validate_password(request.data['old_password']):
-                    user.password = Bcrypt().generate_password_hash(request.data['new_password']).decode()
+                    user.password = Bcrypt().generate_password_hash(
+                        request.data['new_password']).decode()
                     user.save()
 
                     response = {
@@ -172,10 +173,207 @@ class ResetPasswordAPI(MethodView):
             return make_response(jsonify(responseObject)), 403
 
 
+class ShoppingListAPI(MethodView):
+    """This class registers a new user."""
+
+    def post(self):
+        """Handle POST request for this view. Url ---> /shoppinglists/"""
+
+        auth_header = request.headers.get('Authorization')
+        if auth_header:
+            access_token = auth_header.split(" ")[1]
+        else:
+            access_token = ''
+        if access_token:
+            user_id = User.decode_auth_token(access_token)
+            if not isinstance(user_id, str):
+                user = User.query.filter_by(id=user_id).first()
+                if user:
+                    title = str(request.data['title'])
+                    if title:
+                        shopping_list = ShoppingList(
+                            title=title, user_id=user_id)
+                        shopping_list.save()
+                        response = jsonify({
+                            'id': shopping_list.id,
+                            'title': shopping_list.title,
+                            'user_id': user_id
+                        })
+
+                        return make_response(response), 201
+
+                else:
+                    response = {
+                        'status': 'fail',
+                        'message': 'Invalid old password.'
+                    }
+                    return make_response(jsonify(response)), 401
+            else:
+                responseObject = {
+                    'status': 'fail',
+                    'message': user_id
+                }
+                return make_response(jsonify(responseObject)), 401
+        else:
+            responseObject = {
+                'status': 'fail',
+                'message': 'Provide a valid auth token.'
+            }
+            return make_response(jsonify(responseObject)), 403
+
+    def get(self):
+        """Handle GET request for this view. Url ---> /shoppinglists/"""
+
+        auth_header = request.headers.get('Authorization')
+        if auth_header:
+            access_token = auth_header.split(" ")[1]
+        else:
+            access_token = ''
+        if access_token:
+            user_id = User.decode_auth_token(access_token)
+            if not isinstance(user_id, str):
+                user = User.query.filter_by(id=user_id).first()
+                if user:
+                    shopping_lists = ShoppingList.query.filter_by(
+                        user_id=user_id)
+                    results = []
+
+                    for shopping_list in shopping_lists:
+                        obj = {
+                            'id': shopping_list.id,
+                            'title': shopping_list.title,
+                            'user_id': shopping_list.user_id
+                        }
+                        results.append(obj)
+
+                    return make_response(jsonify(results)), 200
+                else:
+                    response = {
+                        'status': 'fail',
+                        'message': 'Invalid old password.'
+                    }
+                    return make_response(jsonify(response)), 401
+            else:
+                responseObject = {
+                    'status': 'fail',
+                    'message': user_id
+                }
+                return make_response(jsonify(responseObject)), 401
+        else:
+            responseObject = {
+                'status': 'fail',
+                'message': 'Provide a valid auth token.'
+            }
+            return make_response(jsonify(responseObject)), 403
+
+
+class ShoppingListIdAPI(MethodView):
+    def get(self, id):
+        # get the access token from the authorization header
+        auth_header = request.headers.get('Authorization')
+        access_token = auth_header.split(" ")[1]
+
+        if access_token:
+            # Get the user id related to this access token
+            user_id = User.decode_auth_token(access_token)
+
+            if not isinstance(user_id, str):
+                # If the id is not a string(error), we have a user id
+                # Get the bucketlist with the id specified from the URL (<int:id>)
+                shoppinglist = ShoppingList.query.filter_by(id=id).first()
+                if not shoppinglist:
+                    # There is no bucketlist with this ID for this User, so
+                    # Raise an HTTPException with a 404 not found status code
+                    abort(404)
+                response = {
+                    'id': shoppinglist.id,
+                    'title': shoppinglist.title,
+                    'user_id': shoppinglist.user_id
+                }
+                return make_response(jsonify(response)), 200
+            else:
+                # user is not legit, so the payload is an error message
+                message = user_id
+                response = {
+                    'message': message
+                }
+                # return an error response, telling the user he is Unauthorized
+                return make_response(jsonify(response)), 401
+
+    def put(self, id):
+        # get the access token from the authorization header
+        auth_header = request.headers.get('Authorization')
+        access_token = auth_header.split(" ")[1]
+
+        if access_token:
+            # Get the user id related to this access token
+            user_id = User.decode_auth_token(access_token)
+
+            if not isinstance(user_id, str):
+                # If the id is not a string(error), we have a user id
+                # Get the bucketlist with the id specified from the URL (<int:id>)
+                shoppinglist = ShoppingList.query.filter_by(id=id).first()
+                if not shoppinglist:
+                    # There is no bucketlist with this ID for this User, so
+                    # Raise an HTTPException with a 404 not found status code
+                    abort(404)
+                new_title = str(request.data['new_title'])
+
+                shoppinglist.title = new_title
+                shoppinglist.save()
+                response = {
+                    'id': shoppinglist.id,
+                    'title': shoppinglist.title,
+                    'user_id': shoppinglist.user_id
+                }
+                return make_response(jsonify(response)), 200
+            else:
+                # user is not legit, so the payload is an error message
+                message = user_id
+                response = {
+                    'message': message
+                }
+                # return an error response, telling the user he is Unauthorized
+                return make_response(jsonify(response)), 401
+
+    def delete(self, id):
+        # get the access token from the authorization header
+        auth_header = request.headers.get('Authorization')
+        access_token = auth_header.split(" ")[1]
+
+        if access_token:
+                # Get the user id related to this access token
+            user_id = User.decode_auth_token(access_token)
+
+            if not isinstance(user_id, str):
+                    # If the id is not a string(error), we have a user id
+                    # Get the bucketlist with the id specified from the URL (<int:id>)
+                shoppinglist = ShoppingList.query.filter_by(id=id).first()
+                if not shoppinglist:
+                    # There is no bucketlist with this ID for this User, so
+                    # Raise an HTTPException with a 404 not found status code
+                    abort(404)
+
+                shoppinglist.delete()
+                return {
+                    "message": "Shopping list {} deleted".format(shoppinglist.id)
+                }, 200
+            else:
+                # user is not legit, so the payload is an error message
+                message = user_id
+                response = {
+                    'message': message
+                }
+                # return an error response, telling the user he is Unauthorized
+                return make_response(jsonify(response)), 401
+
+
 register_api = RegisterAPI.as_view('register_api')
 login_api = LoginAPI.as_view('login_api')
 logout_api = LogoutAPI.as_view('logout_api')
 reset_password_api = ResetPasswordAPI.as_view('reset_password_api')
+shopping_lists_api = ShoppingListAPI.as_view('shopping_lists_api')
+shopping_lists_id_api = ShoppingListIdAPI.as_view('shopping_lists_id_api')
 
 auth_blueprint.add_url_rule(
     '/auth/register',
@@ -196,4 +394,14 @@ auth_blueprint.add_url_rule(
     '/auth/reset-password',
     view_func=reset_password_api,
     methods=['POST']
+)
+shoppinglists_blueprint.add_url_rule(
+    '/shoppinglists/',
+    view_func=shopping_lists_api,
+    methods=['POST', 'GET']
+)
+shoppinglists_blueprint.add_url_rule(
+    '/shoppinglists/<int:id>',
+    view_func=shopping_lists_id_api,
+    methods=['DELETE', 'GET', 'PUT']
 )
